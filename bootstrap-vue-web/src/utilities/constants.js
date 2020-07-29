705172ec58c147b763e0b5b1c5c13227e72096ec
Yasara Peiris <yasara.peiris@uzh.ch>
2020-07-29 16:34:51 +0200

    Feedback - Reformatted

--- bootstrap-vue-web/src/utilities/constants.js
+++ bootstrap-vue-web/src/utilities/constants.js
@@ -1,43 +1,29 @@
 const filterTags = {
-    RUNNING : 'Running',
-    FROM : "From",
-    TO : "To",
-    STATE : "State",
-    MODIFIED_SINCE: "Modified Since",
-    TYPES: 'Types',
-    SORT: "Sort",
-    LIMIT : 'Limit',
-}
-const filterTagsSort = {
-    RUNNING : "1",
-    FROM : "2",
-    TO : "3",
-    STATE : "4",
-    MODIFIED_SINCE: "5",
-    TYPES: "6",
-    LIMIT : "7",
-    SORT: "8",
-}
-const states = {
-    DEFAULT : 'default',
-    TRASHED : 'trashed',
-    ALL : 'all'
-}
-
-const sort = {
-    SORTASCENDING : "sortAscending"
-}
-
-const constants={
-    DEFAULT_SERVICE_INFO_URL :"https://reg.pryv.me/service/info"
-}
-
-export
-{
-    filterTags,
-    states,
-    sort,
-    constants,
-    filterTagsSort
+  RUNNING: "Running",
+  FROM: "From",
+  TO: "To",
+  STATE: "State",
+  MODIFIED_SINCE: "Modified Since",
+  TYPES: "Types",
+  SORT: "Sort",
+  LIMIT: "Limit",
 };
-
+const filterTagsSort = {
+  RUNNING: "1",
+  FROM: "2",
+  TO: "3",
+  STATE: "4",
+  MODIFIED_SINCE: "5",
+  TYPES: "6",
+  LIMIT: "7",
+  SORT: "8",
+};
+const states = {
+  DEFAULT: "default",
+  TRASHED: "trashed",
+  ALL: "all",
+};
+const constants = {
+  DEFAULT_SERVICE_INFO_URL: "https://reg.pryv.me/service/info",
+};
+export { filterTags, states, constants, filterTagsSort };

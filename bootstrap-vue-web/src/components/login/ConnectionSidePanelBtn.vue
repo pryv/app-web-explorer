b2944e6725191985ff35437dbc600b554023d774
Yasara Peiris <yasara.peiris@uzh.ch>
2020-07-29 14:41:54 +0200

    Feedback - Removed comments

--- bootstrap-vue-web/src/components/login/ConnectionSidePanelBtn.vue
+++ bootstrap-vue-web/src/components/login/ConnectionSidePanelBtn.vue
@@ -1,24 +1,18 @@
 <template>
-    <b-row>
-        <b-button  @click="$router.push('login')" class="w-100" style="text-align: left" variant="light">
-            <b-icon icon="plus" font-scale="1.5"></b-icon>
-            Connection
-        </b-button>
-        <!--<b-button @click="$router.push('about')" class="w-100" style="text-align: left" variant="light">
-            <b-icon icon="plus" font-scale="1.5"></b-icon>
-            Login - Api
-        </b-button>
-        <b-button @click="$router.push('test')" class="w-100" style="text-align: left" variant="light">
-            <b-icon icon="plus" font-scale="1.5"></b-icon>
-           Test
-        </b-button>-->
-    </b-row>
+  <b-row>
+    <b-button
+      @click="$router.push('login')"
+      class="w-100"
+      style="text-align: left"
+      variant="light"
+    >
+      <b-icon icon="plus" font-scale="1.5"></b-icon>
+      Connection
+    </b-button>
+  </b-row>
 </template>
 <script>
-    export default {
-        name: 'ConnectionSidePanelBtn',
-        computed:{
-            isLoggedIn(){ return this.$sessionStorage.token && this.$sessionStorage.token !== '' ; }
-        }
-    }
+export default {
+  name: "ConnectionSidePanelBtn",
+};
 </script>

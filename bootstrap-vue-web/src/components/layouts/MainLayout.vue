65496e0cb30be2de9c5f4b24f4002de129bf438d
Yasara Peiris <yasara.peiris@uzh.ch>
2020-07-29 14:33:08 +0200

    Feedback - Removed unnecessary comments

--- bootstrap-vue-web/src/components/layouts/MainLayout.vue
+++ bootstrap-vue-web/src/components/layouts/MainLayout.vue
@@ -1,15 +1,13 @@
 <template>
-    <b-row class="justify-content-center h-100 px-4 align-items-center">
-        <router-view/>
-        <!-- <slot></slot>-->
-        <!--        todo v-slot-->
-    </b-row>
+  <b-row class="justify-content-center h-100 px-4 align-items-center">
+    <router-view />
+  </b-row>
 </template>
 <script>
-    export default {
-        name: 'MainLayout',
-        props: {
-            msg: String
-        }
-    }
+export default {
+  name: "MainLayout",
+  props: {
+    msg: String,
+  },
+};
 </script>

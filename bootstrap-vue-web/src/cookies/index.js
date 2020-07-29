8e909550165c8e7c1ddda783ed427d4f6f09ac44
Yasara Peiris <yasara.peiris@uzh.ch>
2020-07-29 16:25:30 +0200

    Feedback - Reformatted

--- bootstrap-vue-web/src/cookies/index.js
+++ bootstrap-vue-web/src/cookies/index.js
@@ -1,5 +1,3 @@
-import Vue from 'vue'
-import VueCookies from 'vue-cookies'
-Vue.use(VueCookies)
-// Tell Vue to use the plugin
-
+import Vue from "vue";
+import VueCookies from "vue-cookies";
+Vue.use(VueCookies);

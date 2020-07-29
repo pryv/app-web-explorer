89dc6f39e11b416d30dde42593295f1faa9f15f5
Yasara Peiris <yasara.peiris@uzh.ch>
2020-07-29 16:18:38 +0200

    Feedback - Reformatted

--- bootstrap-vue-web/src/components/shared/PryvLabel.vue
+++ bootstrap-vue-web/src/components/shared/PryvLabel.vue
@@ -1,48 +1,46 @@
 <template>
-    <b-row>
-        <b-col cols="1" class="p-0">
-            <a target="_blank" :href="href">
-                <b-img
-                        src="../../assets/link.png"
-                        width="10px"
-                ></b-img>
-            </a>
-        </b-col>
-        <b-col cols="11" style="text-align: left;padding: 0;">
-            <h5 class="pryv-label pl-3 mb-2"
-                :parentData="parentData"
-                v-text="parentData"
-            ></h5>
-        </b-col>
-    </b-row>
+  <b-row>
+    <b-col cols="1" class="p-0">
+      <a target="_blank" :href="href">
+        <b-img src="../../assets/link.png" width="10px"></b-img>
+      </a>
+    </b-col>
+    <b-col cols="11" style="text-align: left;padding: 0;">
+      <h5
+        class="pryv-label pl-3 mb-2"
+        :parentData="parentData"
+        v-text="parentData"
+      ></h5>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
-    export default {
-        name: "Label",
-        props: {
-            parentData: {
-                type: String,
-                default() {
-                    return ''
-                }
-            },
-            href: {
-                type: String
-            }
-        },
-    }
+export default {
+  name: "Label",
+  props: {
+    parentData: {
+      type: String,
+      default() {
+        return "";
+      },
+    },
+    href: {
+      type: String,
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .pryv-label {
-        font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-        display: inline-block;
-        padding: 2px 6px;
-        padding-left: 0 !important;
-        margin-bottom: 0;
-        vertical-align: middle;
-        margin-left: -2%;
-        font-size: 1rem !important;
-    }
+.pryv-label {
+  font-family: "Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  display: inline-block;
+  padding: 2px 6px;
+  padding-left: 0 !important;
+  margin-bottom: 0;
+  vertical-align: middle;
+  margin-left: -2%;
+  font-size: 1rem !important;
+}
 </style>

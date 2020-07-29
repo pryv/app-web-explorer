07a70acb3c56a23234f7f9be3efa616ff000b9c0
Yasara Peiris <yasara.peiris@uzh.ch>
2020-07-29 16:29:10 +0200

    Feedback - Reformatted and removed unused functions

--- bootstrap-vue-web/src/router/index.js
+++ bootstrap-vue-web/src/router/index.js
@@ -1,84 +1,45 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: () => import(/* webpackChunkName: "login" */ '../views/Home.vue'),
-    },
-    {
-        path: '/home',
-        name: 'Home',
-        component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue'),
-    },
-    {
-        path: '/events',
-        name: 'Events',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "login" */ '../views/Events.vue'),
-    },
-    {
-        path: '/info',
-        name: 'Info',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "login" */ '../views/Info.vue'),
-    },
-]
+  {
+    path: "/",
+    name: "Home",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Home.vue"),
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+  },
+  {
+    path: "/events",
+    name: "Events",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Events.vue"),
+  },
+  {
+    path: "/info",
+    name: "Info",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Info.vue"),
+  },
+];
 
 const router = new VueRouter({
-    mode: 'hash', //todo hash or history mode
-    base: process.env.BASE_URL,
-    routes,
-    computed: {}
-})
+  mode: "hash",
+  base: process.env.BASE_URL,
+  routes,
+  computed: {},
+});
 
-// eslint-disable-next-line no-unused-vars
-function isAuthenticated(to, from, next, that) {
-    if (!Vue.$sessionStorage) {
-        next({
-            name: "Home" // back to safety route //
-        });
-    } else {
-        if (Vue.$sessionStorage.connection_arr && Vue.$sessionStorage.access_info_arr) {
-            next({
-                name: "Access" // back to safety route //
-            });
-        } else {
-            next({
-                name: "Home" // back to safety route //
-            });
-        }
-    }
-
-}
-
-// eslint-disable-next-line no-unused-vars
-function isNotAuthenticated(to, from, next) {
-    {
-        if (!sessionStorage.getItem("token")) {
-            next();
-        } else {
-            next({
-                name: "Access" // back to safety route //
-            });
-        }
-    }
-
-}
-
-export default router
+export default router;

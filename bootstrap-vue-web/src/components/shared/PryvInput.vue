7eed70b6359fc0a4c7a6e055168be660d137aea8
Yasara Peiris <yasara.peiris@uzh.ch>
2020-07-30 17:24:50 +0200

    Feedback -  Fixed issues related to the feedback

--- bootstrap-vue-web/src/components/shared/PryvInput.vue
+++ bootstrap-vue-web/src/components/shared/PryvInput.vue
@@ -1,45 +1,37 @@
 <template>
-    <b-input
-            class="pryv-input"
-            :id="id"
-            :placeholder="placeholder"
-            @input="$emit('textInput', $event)"
-            :value="value"
-            :rules="[rules.required]"
-            type="text"
-            :disabled="disabled === true"
-    ></b-input>
+  <b-input
+    class="pryv-input"
+    :id="id"
+    :placeholder="placeholder"
+    @input="$emit('textInput', $event)"
+    :value="value"
+    type="text"
+    :disabled="disabled"
+  ></b-input>
 </template>
 
 <script>
-    export default {
-        name: "PryvInput",
-        props:
-            {
-                value: {type: String, default: ''},
-                placeholder: {type: String},
-                id: {type: String},
-                disabled: {type: Boolean, default: false}
-            },
-        data: () => ({
-            rules: {
-                required: value => !!value || 'This field is required.',
-            },
-        }),
-    }
-
+export default {
+  name: "PryvInput",
+  props: {
+    value: { type: String, default: "" },
+    placeholder: { type: String },
+    id: { type: String },
+    disabled: { type: Boolean, default: false },
+  },
+};
 </script>
 
 <style scoped>
-    .pryv-input {
-        font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-        font-size: 12px;
-        line-height: 20px;
-        text-decoration: none;
-        display: inline-block;
-        padding: 2px 6px;
-        margin-bottom: 0;
-        vertical-align: middle;
-        cursor: pointer;
-    }
+.pryv-input {
+  font-family: "Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 12px;
+  line-height: 20px;
+  text-decoration: none;
+  display: inline-block;
+  padding: 2px 6px;
+  margin-bottom: 0;
+  vertical-align: middle;
+  cursor: pointer;
+}
 </style>

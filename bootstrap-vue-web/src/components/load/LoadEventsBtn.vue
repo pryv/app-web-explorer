edc9465189dd22d668a398e78ccaf08b8b42dc9e
Yasara Peiris <yasara.peiris@uzh.ch>
2020-08-03 11:26:47 +0200

    Feedback - Review finalized - LoadEventsBtn class

--- bootstrap-vue-web/src/components/load/LoadEventsBtn.vue
+++ bootstrap-vue-web/src/components/load/LoadEventsBtn.vue
@@ -4,8 +4,6 @@
 
 <script>
 import PryvBtn from "../shared/PryvBtn";
-import GET_STREAMS_API from "../../utilities/api";
-import ACCESS_INFO_API from "../../utilities/api";
 
 export default {
   name: "LoadEventsBtn",
@@ -37,52 +35,26 @@ export default {
   },
   methods: {
     apiLogin(endPoint) {
-      var connection = null;
+      let connection = null;
       try {
         connection = new this.$pryv.Connection(endPoint);
       } catch (e) {
-        console.log("connection retrieved successfully");
+        console.log("connection retrieved successfully" + e);
+        return;
       }
       return connection;
     },
     loadEvents() {
-      var existing = this.$sessionStorage.endpoint_arr;
+      let existing = this.$sessionStorage.endpoint_arr;
       existing = existing ? JSON.parse(existing) : [];
       existing.forEach(function(obj) {
-        var connection = this.apiLogin(obj.key);
+        const connection = this.apiLogin(obj.key);
         if (connection) this.updateStore(connection);
       });
     },
-    async addStreamsToStore(connection) {
-      try {
-        const result = await connection.api(GET_STREAMS_API.GET_STREAMS_API);
-        if (result)
-          this.streams_map = [connection.apiEndpoint, result[0].streams];
-      } catch (e) {
-        console.log("Error occurred when retrieving streams");
-        console.log(e);
-        return false;
-      }
-      return true;
-    },
-    async addAccessInfoToStore(connection) {
-      try {
-        const result = await connection.api(ACCESS_INFO_API.ACCESS_INFO_API);
-        if (result)
-          this.access_info_map = [
-            connection.token + connection.endpoint,
-            result[0],
-          ];
-      } catch (e) {
-        console.log("Error occurred when retrieving access info");
-        console.log(e);
-        return false;
-      }
-      return true;
-    },
     async addEventsToStore(connection) {
       this.events = [];
-      var queryParams = {};
+      let queryParams = {};
       try {
         const result = await connection.getEventsStreamed(
           queryParams,
@@ -91,8 +63,7 @@ export default {
         this.events_map = [connection.token + connection.endpoint, this.events];
         console.log(result);
       } catch (e) {
-        console.log("Error occurred when retrieving events");
-        console.log(e);
+        console.log("Error occurred when retrieving events" + e);
         return false;
       }
       return true;
@@ -104,10 +75,7 @@ export default {
     },
     updateStore(connection) {
       if (this.addEventsToStore(connection)) this.$router.push("events");
-      return false;
     },
   },
 };
 </script>
-
-<style scoped></style>
